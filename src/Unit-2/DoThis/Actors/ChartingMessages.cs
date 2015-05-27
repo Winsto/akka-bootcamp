d0aa2f033faa1d2391b9ae7f9cb166c7957995a9
Neil Winstanley <winsto@live.co.uk>
2015-05-27 22:32:15 +0100

    Add message definitions.

---
+++ src/Unit-2/DoThis/Actors/ChartingMessages.cs
@@ -0,0 +1,62 @@
+﻿namespace ChartApp.Actors
+{
+    using Akka.Actor;
+
+    public sealed class ChartingMessages
+    {
+        public class GatherMetrics { }
+
+        public class Metric
+        {
+            public Metric(string seriesName, float counterValue)
+            {
+                SeriesName = seriesName;
+
+                CounterValue = counterValue;
+            }
+
+            public string SeriesName{get; private set;}
+
+            public float CounterValue { get; private set;  }
+        }
+
+        public enum CounterType
+        {
+            Cpu,
+            Memory,
+            Disk
+        }
+
+        public class SubscriberCounter
+        {
+            public SubscriberCounter(CounterType counter, IActorRef subscriber)
+            {
+                Counter = counter;
+
+                Subscriber = subscriber;
+            }
+
+            public CounterType Counter
+            { get; private set; }
+
+            public IActorRef Subscriber
+            { get; private set; }
+        }
+
+        public class UnsubscriberCounter
+        {
+            public UnsubscriberCounter(CounterType counter, IActorRef subscriber)
+            {
+                Subscriber = subscriber;
+                Counter = counter;
+            }
+
+            public CounterType Counter
+            { get; private set; }
+
+            public IActorRef Subscriber
+            { get; private set; }
+        }
+    }
+
+}
