5d9f9f4ebed7e41ba082f018d2d6340974e6f2fa
Neil.Winstanley <>
2015-05-28 15:32:28 +0100

    Remove redundant using.

--- src/Unit-2/DoThis/Main.cs
+++ src/Unit-2/DoThis/Main.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
-using System.Windows.Forms.DataVisualization.Charting;
 using Akka.Actor;
 using Akka.Util.Internal;
 using ChartApp.Actors;
